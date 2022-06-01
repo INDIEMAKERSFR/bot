214f5df4904ff45c8d730fed99a6bdc18ba7288a
martindonadieu <martindonadieu@gmail.com>
2022-06-02 00:21:14 +0100

    fix: add back projectFn

--- netlify/services/discord/bot/index.ts
+++ netlify/services/discord/bot/index.ts
@@ -1,33 +1,43 @@
 import { InteractionType } from 'discord-interactions'
 import { ApplicationCommandInteractionDataOption, Interaction } from '../command'
-// import { userFn } from './user'
-// import { incomeFn } from './incomes'
-// import { projectFn } from './project'
 // import { sendTxtLater } from './utils'
+import { userFn } from './user'
+import { incomeFn } from './incomes'
+import { projectFn } from './project'
 import { karmaFn } from './karma'
-// import { taskFn } from './tasks'
-// import { postFn } from './post'
+import { taskFn } from './tasks'
+import { postFn } from './post'
 
-const im = (option: ApplicationCommandInteractionDataOption, senderId: string): Promise<any> => {
+const im = (interaction: Interaction, option: ApplicationCommandInteractionDataOption, senderId: string): Promise<any> => {
   try {
     if (option.name === 'karma' && option.options && option.options.length > 0) {
       return karmaFn(option.options[0], senderId)
     }
-    // if (option.name === 'projet' && option.options && option.options.length > 0) {
-    //   return projectFn(option.options[0], senderId)
-    // }
-    // if (option.name === 'post' && option.options && option.options.length > 0) {
-    //   return postFn(option.options[0], senderId)
-    // }
-    // if (option.name === 'tache' && option.options && option.options.length > 0) {
-    //   return taskFn(option.options[0], senderId)
-    // }
-    // if (option.name === 'revenu' && option.options && option.options.length > 0) {
-    //   return incomeFn(option.options[0], senderId)
-    // }
-    // if (option.name === 'maker' && option.options && option.options.length > 0) {
-    //   return userFn(option.options[0], senderId)
-    // }
+    if (option.name === 'projet' && option.options && option.options.length > 0) {
+      return projectFn(interaction, option.options[0], senderId).then(() => ({
+        content: "..."
+      }))
+    }
+    if (option.name === 'post' && option.options && option.options.length > 0) {
+      return postFn(interaction, option.options[0], senderId).then(() => ({
+        content: "..."
+      }))
+    }
+    if (option.name === 'tache' && option.options && option.options.length > 0) {
+      return taskFn(interaction, option.options[0], senderId).then(() => ({
+        content: "..."
+      }))
+    }
+    if (option.name === 'revenu' && option.options && option.options.length > 0) {
+      return incomeFn(interaction, option.options[0], senderId).then(() => ({
+        content: "..."
+      }))
+    }
+    if (option.name === 'maker' && option.options && option.options.length > 0) {
+      return userFn(interaction, option.options[0], senderId).then(() => ({
+        content: "..."
+      }))
+    }
     if (option.name === 'doc') {
       return Promise.resolve({ content: `Voici la doc pou m'utiliser ! https://doc.indiemakers.fr` })
     }
@@ -42,7 +52,7 @@ const discordInteraction = (interaction: Interaction): Promise<any> => {
   // console.log('discordInteraction', interaction)
   if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND && interaction.data) {
     if (interaction.data.name === 'im' && interaction.data.options && interaction.data.options.length > 0 && interaction.member.user) {
-      return im(interaction.data.options[0], interaction.member.user.id)
+      return im(interaction, interaction.data.options[0], interaction.member.user.id)
     }
     return Promise.resolve({ content: `La Commande ${interaction.data.name} n'est pas pris en charge 🤫` })
   }
